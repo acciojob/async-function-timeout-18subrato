536cbd03da20cf3437b6080d20b0bebb3a2d414b
Subrato Kumar Verma <138965935+18subrato@users.noreply.github.com>
2025-02-25 23:57:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+let inputBox = document.querySelector("#text");
+let delayBox = document.querySelector("#delay");
+let btn = document.querySelector("#btn");
+
+function hello(){
+	let output = document.querySelector("#output");
+	output.innerText = inputBox.value;
+}
+btn.addEventListener("click",function(){
+	setTimeout(hello,delayBox.value);
+})
